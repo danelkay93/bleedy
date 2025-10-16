df02a37f077a07954aec698d25bf37c917f9c62c
codefactor-io <support@codefactor.io>
2025-10-16 08:24:54 +0000

    [CodeFactor] Apply fixes to commit 75ec6ba

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,5 +1,10 @@
 <template>
-  <svg ref="svg" viewBox="0 0 100 120" width="100%" height="100%" />
+  <svg
+    ref="svg"
+    viewBox="0 0 100 120"
+    width="100%"
+    height="100%"
+  />
 </template>
 
 <script lang="ts">
